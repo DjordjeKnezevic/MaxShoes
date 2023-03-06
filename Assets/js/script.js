760f24ff4ebabc54d06fae3508ed317fef6eb024
Djordje Knezevic <misterja@protonmail.com>
2023-03-06 01:58:56 +0100

    Sredjen JSON

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -482,10 +482,16 @@ window.onload = async function () {
             for (let shoe of shoeList) {
                 patikeDisplay.innerHTML += `
                 <article class="shoe mt-4 container">
-                <img src="Assets/img/shoes/Shoe1.jpg" alt="Shoe1">
-                <strong>Asics Metaspeed Sky</strong>
-                <i>Men</i>
-                <p>$250</p>
+                <img src="${shoe.img.src}" alt="${shoe.img.alt}" class="img-fluid">
+                <strong>${shoe.brand} ${shoe.model}</strong>
+                <i>${shoe.category}</i>
+                <div class="d-flex justify-content-between w-50">
+                ${shoe.price.oldPrice ? 
+                    "<p class='text-decoration-line-through'>$" + shoe.price.oldPrice + "</p>" + "<p>$" + shoe.price.currentPrice + "</p>" :
+                    "<p>$" + shoe.price.currentPrice + "</p>"
+                }
+
+                    </div>
                 <a data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Add to cart">
                 <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-cart3 korpa" viewBox="0 0 16 16" data-bs-title="Remove from cart">
         <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"></path></svg></a></article>
